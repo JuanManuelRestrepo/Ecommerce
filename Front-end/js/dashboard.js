85dd8bd25da7aa1f9b560b1ddabc5556da808bdb
Estebanvd-131 <villaed330@gmail.com>
2024-11-06 20:57:35 -0500

    Add files via upload

--- Front-end/js/dashboard.js
+++ Front-end/js/dashboard.js
@@ -42,6 +42,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function checkLoginStatus() {
 
+
+
+    
     const isLoggedIn = sessionStorage.getItem('isLoggedIn');
     if (!isLoggedIn) {
         window.location.href = 'index.html';
